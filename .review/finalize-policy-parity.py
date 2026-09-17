c90ffa56766e4bdd9b5d6b084e07a07b4a920efe
senegrom <6349874+senegrom@users.noreply.github.com>
2026-09-17 18:03:54 +0100

    Anchor cloud metadata edit independently of validation call

--- .review/finalize-policy-parity.py
+++ .review/finalize-policy-parity.py
@@ -3,7 +3,10 @@ p = Path('.review/apply-policy-parity.py')
 s = p.read_text()
 start = s.index("    part = rep(part, '\"audit_share\": audit_share,',")
 end = s.index('    return s[:begin]', start)
-s = s[:start] + '''    part = rep(part, 'extra_candidates=extra_candidates, audit_share=audit_share,',
-               'extra_candidates=extra_candidates, audit_share=audit_share, rollout_batch=rollout_batch,\\n                        policy_inference_rule=dict(version=1, cuda_46="bfloat16", other="float32", tie_break="first_policy_index"),')
-''' + s[end:]
+old = 'extra_candidates=extra_candidates, audit_share=audit_share,\n                        placement_head_sha256='
+new = ('extra_candidates=extra_candidates, audit_share=audit_share, rollout_batch=rollout_batch,\n'
+       '                        policy_inference_rule=dict(version=1, cuda_46="bfloat16", other="float32", tie_break="first_policy_index"),\n'
+       '                        placement_head_sha256=')
+replacement = '    part = rep(part, ' + repr(old) + ',\n               ' + repr(new) + ')\n'
+s = s[:start] + replacement + s[end:]
 exec(compile(s, str(p), 'exec'))
